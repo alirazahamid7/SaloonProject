9fa6716264cfd722a38fe5e1a1fc1baeda0a65ea
Ali Raza <alirazahamid7@gmail.com>
2023-02-08 16:17:27 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './Component/Test';
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        hello world
+      </h1>
+      <Test/>
+    </div>
+  
+  );
+}
+
+export default App;
---
+++ src/Component/Test.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Test() {
+  return (
+    <div>
+      <h1>Hello Import</h1>
+    </div>
+  )
+}
+export default Test
